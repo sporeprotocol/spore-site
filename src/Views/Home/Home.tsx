15ca6c240653abfc97fe4107da0d015b33d8017f
lee920217 <francislee920217@gmail.com>
2023-08-15 17:53:26 +0800

    add animation

--- src/Views/Home/Home.tsx
+++ src/Views/Home/Home.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import styles from './index.module.scss'
+import React from 'react';
+import styles from './index.module.scss';
 import HomeSlogan from "../../components/HomeSlogan/HomeSlogan";
 import SporeIntro from "../../components/SporeIntro/SporeIntro";
 import SporeGuide from "../../components/SporeGuide/SporeGuide.tsx";
 import CommonFooter from "../../components/CommonFooter/CommonFooter.tsx";
+import { ReactComponent as Star } from "../../assets/svg/star.svg";
+
+const SvgItem: React.FC = () => (
+    <div className={styles.SvgItem}>
+        <Star />
+        Meme
+    </div>
+);
+
+const SVG_ITEM_COUNT = 15;
 
 const Home: React.FC = () => {
     return (
@@ -11,8 +21,14 @@ const Home: React.FC = () => {
             <div className={styles.HomePageWrapper}>
                 <HomeSlogan />
                 <SporeIntro />
-                <div className={styles.tmp}>
-
+                <div className={styles.MemeContainer}>
+                    <div className={styles.SvgContainer}>
+                        {Array.from({ length: SVG_ITEM_COUNT * 2 }).map((_, idx) => <SvgItem key={idx} />)}
+                    </div>
+                    <div className={styles.MemeBackground}></div>
+                    <div className={styles.SvgContainer}>
+                        {Array.from({ length: SVG_ITEM_COUNT * 2 }).map((_, idx) => <SvgItem key={idx} />)}
+                    </div>
                 </div>
                 <SporeGuide />
             </div>
@@ -21,4 +37,4 @@ const Home: React.FC = () => {
     )
 }
 
-export default Home
+export default Home;
