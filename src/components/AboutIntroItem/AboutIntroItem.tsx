59dcf4e8c7eaf4c86a05fb87cdfb41f23816af45
lee920217 <francislee920217@gmail.com>
2023-08-17 21:20:42 +0800

    udpate data

--- src/components/AboutIntroItem/AboutIntroItem.tsx
+++ src/components/AboutIntroItem/AboutIntroItem.tsx
@@ -1,29 +1,29 @@
-import React, { ReactElement } from 'react';
-import styles from './index.module.scss';
+    import React, { ReactElement } from 'react';
+    import styles from './index.module.scss';
 
-interface Props {
-    svg: ReactElement;
-    title: string;
-    subtitle: string;
-    content: string;
-    index: number;
-}
+    interface Props {
+        svg: ReactElement;
+        title: string;
+        subtitle: string;
+        content: string;
+        index: number;
+    }
 
-const AboutIntroItem: React.FC<Props> = ({ svg, title, subtitle, content, index }) => {
-    const isEven = index % 2 === 0;
+    const AboutIntroItem: React.FC<Props> = ({ svg, title, subtitle, content, index }) => {
+        const isEven = index % 2 === 0;
 
-    return (
-        <div className={isEven ? styles.evenWrapper : styles.oddWrapper}>
-            <div className={styles.svgWrapper}>
-                {svg}
+        return (
+            <div className={isEven ? styles.evenWrapper : styles.oddWrapper}>
+                <div className={styles.svgWrapper}>
+                    {svg}
+                </div>
+                <div className={styles.textWrapper}>
+                    <div className={`${styles.title} sub-header`}>{title}</div>
+                    <h3 className={styles.subtitle}>{subtitle}</h3>
+                    <p className={`${styles.content} body-1`}>{content}</p>
+                </div>
             </div>
-            <div className={styles.textWrapper}>
-                <div className={`${styles.title} sub-header`}>{title}</div>
-                <h3 className={styles.subtitle}>{subtitle}</h3>
-                <p className={`${styles.content} body-1`}>{content}</p>
-            </div>
-        </div>
-    );
-}
+        );
+    }
 
-export default AboutIntroItem;
+    export default AboutIntroItem;
