07c165c2f71615d808937aabd80467ea6cb11d77
lee920217 <francislee920217@gmail.com>
2023-11-08 22:21:49 +0800

    fixed conflict

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-<<<<<<< HEAD
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -74,19 +73,5 @@ const config: Config = {
     },
   },
   plugins: [],
-=======
-    content: [
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-        extend: {
-            colors: {
-            },
-        },
-    },
-    plugins: [],
->>>>>>> master
 }
 export default config
