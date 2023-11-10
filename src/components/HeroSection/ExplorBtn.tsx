e28b489e15e51593a4e2cad6f8fcff35716eab40
lee920217 <francislee920217@gmail.com>
2023-11-10 16:51:43 +0800

    updated mobile problem

--- src/components/HeroSection/ExplorBtn.tsx
+++ src/components/HeroSection/ExplorBtn.tsx
@@ -5,7 +5,7 @@ import {DocUrl} from "@/data/links";
 const ExploreBtn: React.FC = () => {
     return (
         <Link href={`${DocUrl.baseUrl}`} target={'_blank'}>
-            <div className="cursor-pointer flex justify-center items-center shadow-custom w-[263px] rounded-full h-[56px] p-[10px 24px] gap-[8px] text-button font-custom font-extrabold text-white bg-BrandBlack">
+            <div className="sm:mt-12 cursor-pointer flex justify-center items-center shadow-custom w-[263px] rounded-full h-[56px] p-[10px 24px] gap-[8px] text-button font-custom font-extrabold text-white bg-BrandBlack">
                 {`Explore Documentation >`}
             </div>
         </Link>
