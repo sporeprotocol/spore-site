2e4230b6a04628ce999242300b6520e4ec534d6f
anni <112425356+anipenguin@users.noreply.github.com>
2024-02-07 20:46:19 -0800

    Update DC Link.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -50,7 +50,7 @@ const Footer: React.FC = () => {
                                 src={'/svg/github-pc.svg'} alt={'logo'}
                             />
                         </Link>
-                        <Link target={'_blank'} href={'https://discord.com/channels/1083998986735730738/1176755433462054973'} className="rounded-full bg-white hover:bg-BackgroundDark">
+                        <Link target={'_blank'} href={'https://discord.gg/X7D7xG7YeX'} className="rounded-full bg-white hover:bg-BackgroundDark">
                             <Image
                                 width={48}
                                 height={48}
